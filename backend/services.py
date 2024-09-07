fdb7d0cb668def5fc5ba50d15b05382848073c51
Isaac Vieira <isaacfiuzavieira@gmail.com>
2024-09-06 21:27:30 -0300

    feat: bedrock integration

--- backend/services.py
+++ backend/services.py
@@ -5,6 +5,7 @@ class Services:
 
     def __init__ (self, region: str = 'us-east-1', profile: str = 'tf-user'):
         self.session = boto3.Session(profile_name=profile, region_name=region)
+        self.model_id = "anthropic.claude-v2"
 
     def get_celebrity(self, image: bytes):
 
@@ -22,4 +23,26 @@ class Services:
     def get_completion(self, celebrities: list):
         client = self.session.client('bedrock-runtime')
 
-        pass
+        celebrities = ", ".join(celebrities)
+
+        prompt = f"Escreva um pequeno parágrafo contando uma curiosidade sobre a(s) celebridade(s) {celebrities} "
+        
+        body = {
+            "anthropic_version": "bedrock-2023-05-31",
+            "max_tokens": 250,
+            "temperature": 0.9,
+            "messages": [
+                {
+                    "role": "user",
+                    "content": [{"type": "text", "text": prompt}],
+                }
+            ],
+        }   
+
+        body = json.dumps(body)
+
+        response = client.invoke_model(modelId=self.model_id, body=body)
+        model_response = json.loads(response["body"].read())
+        response_text = model_response["content"][0]["text"]
+
+        return response_text
